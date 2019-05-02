cb6cab674660b2367290794a1ba13db48f641ec2
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-05-02 18:40:52 +0900

    Until assert with chai

--- test/specs/sample.js
+++ test/specs/sample.js
@@ -1,5 +1,8 @@
+const expect = require("chai").expect
+
 describe("Sample", function () {
   it('can get title', function () {
     browser.url("/");
+    expect(browser.getTitle()).to.eq("WebdriverIO · Next-gen WebDriver test framework for Node.js");
   })
 });
