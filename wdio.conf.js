e040a219e71b711f08541ccafc210a205cf29d1a
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-05-20 14:27:38 +0900

    Firefox

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,7 +51,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 1,
         //
-        browserName: 'chrome',
+        browserName: 'firefox',
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
