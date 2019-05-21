1401f477c47e3041f1bebf664b02769b15d940eb
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-05-21 18:52:44 +0900

    Run safari

--- config/wdio.chrome.conf.js
+++ config/wdio.chrome.conf.js
@@ -1,7 +1,6 @@
 const config = require("./wdio.base.conf").config;
 
 config.capabilities = [{
-  maxInstances: 1,
   browserName: 'chrome',
   'goog:chromeOptions': {
     args: ['window-size=1920,1080', '--headless', '--disable-gpu']
--- config/wdio.safari.conf.js
+++ config/wdio.safari.conf.js
@@ -1,10 +1,7 @@
 const config = require("./wdio.base.conf").config;
 
 config.capabilities = [{
-  browserName: 'firefox',
-  'moz:firefoxOptions': {
-    args: ['window-size=1920,1080', '--headless']
-  }
+  browserName: 'safari',
 }]
 
 exports.config = config;
