ffb1bb55a47094c689b2b7bea992221391963d23
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-05-26 16:18:13 +0900

    Output junit

--- config/wdio.base.conf.js
+++ config/wdio.base.conf.js
@@ -109,7 +109,16 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    // reporters: ['dot'],
+    reporters: [
+        ['junit',
+            {
+                outputDir: './reporter',
+                outputFileFormat: function (options) {
+                    return `results-${options.capabilities.browserName}.xml`
+                }
+            }
+        ]
+    ],
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
