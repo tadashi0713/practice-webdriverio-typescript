e82c3bf5230ca85a84e8120495e99abcf521b6e0
tadashi0713 <tadashi.nemoto0713@gmail.com>
2019-05-26 15:37:37 +0900

    Add capabilities

--- config/wdio.edge.conf.js
+++ config/wdio.edge.conf.js
@@ -2,6 +2,8 @@ const config = require("./wdio.base.conf").config;
 
 config.capabilities = [{
   browserName: 'MicrosoftEdge',
+  osPlatform: 'Windows',
+  osVersion: '10'
 }]
 
 exports.config = config;
